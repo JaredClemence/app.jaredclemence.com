22d2b09c0744700331e1740b018fd8c20a1fc26b
Jared R. Clemence <Jared R. Clemence@JRC_2024_Dell>
2025-01-03 05:26:00 -0800

    Corrected error in route table.

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,5 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-    
-    Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index']);
+    Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
 });
